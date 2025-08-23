7c4f40061fee1d7ec29f95e0bc4f072075de5567
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2025-08-23 09:45:42 +0500

    Changes the about text

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <section className="container pt-2 mt-2 mt-md-4 mt-lg-5">
         <div className="row pt-xl-3">
           <div className="col-md-5 text-center text-md-start pb-5">
-            <h1 className="mb-4">About Urban Club Shop</h1>
+           <h2 className="display-4 mb-0 text-center"><span className="text-gradient-primary">Urban Panel Shop</span></h2>
             <p className="fs-lg pb-lg-3 mb-4">We create diverse, complex, web and mobile solutions for any business need. With us you get quality software and perfect service every time.</p>
             <a href="about-v2.html" className="btn btn-primary shadow-primary btn-lg">More About Us</a>
             <div className="mt-3 mt-sm-0 pt-4 pt-sm-5 mt-xl-4">
