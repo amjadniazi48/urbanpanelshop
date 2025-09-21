eff268ef9ebd8206915f2529584476837bc2093f
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2025-09-22 00:08:05 +0500

    Changes the pathname in next.config.js file

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,26 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    // Remove the deprecated 'domains' property
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**', // Allows any pathname under the domain
-      },
-      {
-        protocol: 'https',
-        hostname: 'img.youtube.com', // Fixed: moved from domains to remotePatterns
-        pathname: '**', // More flexible than just '/vi/**'
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.ytimg.com', // YouTube thumbnail domain
-        pathname: '/vi/**',
-      },
-    ],
-  },
+ images: {
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "res.cloudinary.com",
+      pathname: "/**", // ✅ must start with /
+    },
+    {
+      protocol: "https",
+      hostname: "img.youtube.com",
+      pathname: "/**",
+    },
+    {
+      protocol: "https",
+      hostname: "i.ytimg.com",
+      pathname: "/vi/**",
+    },
+  ],
+},
+
 };
 
 export default nextConfig;
