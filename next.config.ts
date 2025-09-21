8d2a2a1f9d253ace015e6d61f696066f6b8b8305
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2025-09-22 00:13:38 +0500

    Changes in config file again

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,30 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- images: {
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "res.cloudinary.com",
-      pathname: "/**", // ✅ must start with /
-    },
-    {
-      protocol: "https",
-      hostname: "img.youtube.com",
-      pathname: "/**",
-    },
-    {
-      protocol: "https",
-      hostname: "i.ytimg.com",
-      pathname: "/vi/**",
-    },
-  ],
-},
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+        pathname: "/vi/**",
+      },
+      {
+        protocol: "https",
+        hostname: "capabale-cat-c408f012a1.media.strapiapp.com", // ✅ Strapi media CDN
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
