8f53d7728e9eac04e8f2792a476859b3318a6ad3
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2025-09-25 19:56:16 +0500

    Again changes the next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-     remotePatterns: [ new URL("https://res.cloudinary.com/dlcgduiez/**") ],
-  }, 
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/dlcgduiez/**", // adjust if your Cloudinary folder name differs
+      },
+    ],
+  },
 };
 
 export default nextConfig;
