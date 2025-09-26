d91bb10eb1678d52816d07c5225b0eab9bf701f8
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2025-09-26 23:10:55 +0500

    Change the next.config.ts to next.config.js

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // Good practice for development [^3]
+  images: {
+    // Remove the deprecated 'domains' property
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**', // Allows any pathname under the domain
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com', // Fixed: moved from domains to remotePatterns
+        pathname: '**', // More flexible than just '/vi/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com', // YouTube thumbnail domain
+        pathname: '/vi/**',
+      },
+    ],
+  },
+
+    
+  };
+  
+  export default nextConfig;
+  
