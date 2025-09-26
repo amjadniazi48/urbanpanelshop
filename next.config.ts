4370d69e1b020d50e127ade7d718711c5b495289
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2025-09-26 21:57:47 +0500

    Again change the next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import { NextConfig } from "next";
+const nextConfig: NextConfig = {
   images: {
    // For Next.js 15.3.0+
     remotePatterns: [
